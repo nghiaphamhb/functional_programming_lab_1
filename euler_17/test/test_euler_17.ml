b5460f945f95a712affd4c3b03a006d51cd63f39
second <phamdangtrungnghia@gmail.com>
2025-10-14 14:01:06 +0300

    test: update test

--- euler_17/test/test_euler_17.ml
+++ euler_17/test/test_euler_17.ml
@@ -1,16 +1,29 @@
 open OUnit2
 open Euler_lib
+open Num_to_words
+
+let pp_int = string_of_int
 
 let test_letters _ =
-  assert_equal 23 (Num_to_words.letters 342);
-  assert_equal 20 (Num_to_words.letters 115)
+  assert_equal ~printer:pp_int 23 (letters 342);
+  assert_equal ~printer:pp_int 20 (letters 115)
 
-let test_sum_fold _ =
-  let sum = Fold.sum_fold 1 5 in
-  assert_equal 19 sum
+let test_sum_list_1_5 _ =
+  let xs = Seq_generation.ints_list_map 1 5 |> Filter.keep_valid_ints_list in
+  let sum = Fold.sum_letters_list xs in
+  assert_equal ~printer:pp_int 19 sum
+
+let test_sum_seq_1_5 _ =
+  let s = Seq_generation.ints_seq_map 1 5 |> Filter.keep_valid_ints_seq in
+  let sum = Lazy_seq.sum_letters_seq s in
+  assert_equal ~printer:pp_int 19 sum
 
 let suite =
   "Euler17 Tests"
-  >::: [ "letters" >:: test_letters; "sum_fold" >:: test_sum_fold ]
+  >::: [
+         "letters_342_115" >:: test_letters;
+         "sum_list_1_5" >:: test_sum_list_1_5;
+         "sum_seq_1_5" >:: test_sum_seq_1_5;
+       ]
 
 let () = run_test_tt_main suite
