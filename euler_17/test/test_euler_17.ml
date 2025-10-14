37a6065a450617cf946567bd9dbc88d1fc762292
second <phamdangtrungnghia@gmail.com>
2025-10-14 13:42:01 +0300

    test: update test

--- euler_17/test/test_euler_17.ml
+++ euler_17/test/test_euler_17.ml
@@ -5,12 +5,12 @@ let test_letters _ =
   assert_equal 23 (Num_to_words.letters 342);
   assert_equal 20 (Num_to_words.letters 115)
 
-let test_sum_fold_map _ =
-  let sum = Fold_map.sum_fold_map 1 5 in
+let test_sum_fold _ =
+  let sum = Fold.sum_fold 1 5 in
   assert_equal 19 sum
 
 let suite =
   "Euler17 Tests"
-  >::: [ "letters" >:: test_letters; "sum_fold_map" >:: test_sum_fold_map ]
+  >::: [ "letters" >:: test_letters; "sum_fold" >:: test_sum_fold ]
 
 let () = run_test_tt_main suite
