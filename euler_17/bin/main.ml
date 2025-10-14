7ca8c57efa2b65261f0502a013567db3c66bcb65
second <phamdangtrungnghia@gmail.com>
2025-10-14 13:43:33 +0300

    chore: update main

--- euler_17/bin/main.ml
+++ euler_17/bin/main.ml
@@ -1,14 +1,34 @@
 open Euler_lib
+open Printf
 
-let () =
-  let a, b = (1, 1000) in
-  let ans_tail = Tail_recursion.sum_tail a b in
-  let ans_rec = Recursion.sum_recursive a b in
-  let ans_fold = Fold_map.sum_fold_map a b in
-  let ans_seq = try Lazy_seq.sum_seq a b with _ -> -1 in
+let a, b = (1, 1000)
 
-  Printf.printf "[Tail Recursion ] Answer: %d\n" ans_tail;
-  Printf.printf "[Recursion      ] Answer: %d\n" ans_rec;
-  Printf.printf "[Fold/Map       ] Answer: %d\n" ans_fold;
-  if ans_seq >= 0 then Printf.printf "[Lazy Seq       ] Answer: %d\n" ans_seq
-  else Printf.printf "[Lazy Seq       ] (skipped - Seq not available)\n"
+(* == First block : Begin == *)
+let sum_tail a b =
+  let open Num_to_words in
+  let rec loop acc n = if n > b then acc else loop (acc + letters n) (n + 1) in
+  loop 0 a
+
+let ans_tail = sum_tail a b
+let () = printf "Answer [Tail Recursion ]: %d\n" ans_tail
+(* == First block : End == *)
+
+(* == Second block : Begin == *)
+let rec sum_recursive a b =
+  let open Num_to_words in
+  if a > b then 0 else letters a + sum_recursive (a + 1) b
+
+let ans_rec = sum_recursive a b
+let () = printf "Answer [Recursion      ]: %d\n" ans_rec
+(* == Second block : End == *)
+
+(* == Third block : Begin == *)
+let numbers_list =
+  Seq_generation.ints_list_map a b |> Filter.keep_valid_ints_list
+
+let numbers_seq = Seq_generation.ints_seq_map a b |> Filter.keep_valid_ints_seq
+let ans_fold = Fold.sum_letters_list numbers_list
+let ans_seq = Lazy_seq.sum_letters_seq numbers_seq
+let () = printf "Answer [Fold/Reduce    ]: %d\n" ans_fold
+let () = printf "Answer [Lazy Seq       ]: %d\n" ans_seq
+(* == Third block : End == *)
